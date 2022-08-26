f7b79b605de3881c9b7ce8ddcd6e6c93723e8aa4
Callum Slatter <callum@liftedcare.com>
2022-08-26 12:35:31 +0100

    feat: referenceExtractor
    - can extract valid references from an input string, detect for uniqueness and add to array

--- referencing-system.js
+++ referencing-system.js
@@ -1,9 +1,40 @@
 // read the text in
-var references = [];
+var referenceLibrary = [];
 var inputText = "Mollitia quasi dolorem molestiae ut est voluptates quidem.[Fitt 2011] Natus sit dolore eveniet modi dolores dolore.[Auchinleck 1992] [Wix 2012] Voluptatem vel vel officiis recusandae hic. Sit esse eaque quisquam provident odit et quis nostrum. Dolores ea maiores.[Elcom 2004] [Lindroos 2007] [O'Sheeryne 2004] Iste aut deleniti maiores aliquam asperiores illum consectetur.[Lindroos 2007] [Webben 1987] [Smithin 2004] [Brambill 2001] Ut in et voluptatem sit odit laborum. Veritatis aut reiciendis quasi mollitia esse qui.[Auchinleck 1992] [Wix 2012] [Lindroos 2007] [Smithin 2004] [Brambill 2001] [Varvell 2011]";
 function referenceExtractor(inputText) {
-    var regex = "[(.+?)]";
-    var indexOfFirst = inputText.search(regex);
-    console.log(indexOfFirst);
+    var referenceIdentificationRegex = /\[(.+?)\]/g;
+    var squareBracketRegex = /[\[\]]/g;
+    // Extract all references from the text
+    var rawReferences = inputText.match(referenceIdentificationRegex);
+    if (typeof rawReferences === null) {
+        throw new Error("No matching expressions found.");
+    }
+    var extractedReferences = [];
+    // Remove square brackets from all extracted references and append to extractedReferences array
+    rawReferences === null || rawReferences === void 0 ? void 0 : rawReferences.forEach(function (reference) {
+        var debracketedReference = reference.replace(squareBracketRegex, "");
+        extractedReferences.push(debracketedReference);
+    });
+    return extractedReferences;
 }
-referenceExtractor(inputText);
+function referenceLibraryBuilder(extractedReferences) {
+    extractedReferences.forEach(function (inputReference) {
+        // Could be extracted into its own function
+        var referenceExists = referenceLibrary.some(function (reference) {
+            if (reference.nameYear === inputReference) {
+                return true;
+            }
+            return false;
+        });
+        if (!referenceExists) {
+            var reference = {
+                id: referenceLibrary.length + 1,
+                nameYear: inputReference
+            };
+            referenceLibrary.push(reference);
+        }
+    });
+}
+var extractedReferences = referenceExtractor(inputText);
+referenceLibraryBuilder(extractedReferences);
+console.log(referenceLibrary);
--- referencing-system.ts
+++ referencing-system.ts
@@ -1,40 +1,74 @@
-// read the text in
-
-// first pass 
-
-// create a references array
-// if reference identified
-    // check if reference already exists
-    // if not create a reference and create a reference id
-
 // second pass - with a copy of the text
 
-// if reference identified 
-    // add to a mini array for formatting purposes
-    // check if the next non space character is a valid reference too
-    // If it is, add it to the mini array for formatting purposes  
-    // If not, then run mini array through formatter
-        // Apply the rules laid out in the brief 
-        // Replace the references with the formatted arrays
+// if reference identified
+// add to a mini array for formatting purposes
+// check if the next non space character is a valid reference too
+// If it is, add it to the mini array for formatting purposes
+// If not, then run mini array through formatter
+// Apply the rules laid out in the brief
+// Replace the references with the formatted arrays
 
-
-
-
-interface Reference {
-    id: number;
-    nameYear: string;
+interface IReference {
+  id: number;
+  nameYear: string;
 }
 
-const references: Reference[] = []
+const referenceLibrary: IReference[] = [];
 
-const inputText: string = "Mollitia quasi dolorem molestiae ut est voluptates quidem.[Fitt 2011] Natus sit dolore eveniet modi dolores dolore.[Auchinleck 1992] [Wix 2012] Voluptatem vel vel officiis recusandae hic. Sit esse eaque quisquam provident odit et quis nostrum. Dolores ea maiores.[Elcom 2004] [Lindroos 2007] [O'Sheeryne 2004] Iste aut deleniti maiores aliquam asperiores illum consectetur.[Lindroos 2007] [Webben 1987] [Smithin 2004] [Brambill 2001] Ut in et voluptatem sit odit laborum. Veritatis aut reiciendis quasi mollitia esse qui.[Auchinleck 1992] [Wix 2012] [Lindroos 2007] [Smithin 2004] [Brambill 2001] [Varvell 2011]"
+// Declaring regexes
+const referenceIdentificationRegex = /\[(.+?)\]/g;
+const squareBracketRegex = /[\[\]]/g;
+
+const inputText: string =
+  "Mollitia quasi dolorem molestiae ut est voluptates quidem.[Fitt 2011] Natus sit dolore eveniet modi dolores dolore.[Auchinleck 1992] [Wix 2012] Voluptatem vel vel officiis recusandae hic. Sit esse eaque quisquam provident odit et quis nostrum. Dolores ea maiores.[Elcom 2004] [Lindroos 2007] [O'Sheeryne 2004] Iste aut deleniti maiores aliquam asperiores illum consectetur.[Lindroos 2007] [Webben 1987] [Smithin 2004] [Brambill 2001] Ut in et voluptatem sit odit laborum. Veritatis aut reiciendis quasi mollitia esse qui.[Auchinleck 1992] [Wix 2012] [Lindroos 2007] [Smithin 2004] [Brambill 2001] [Varvell 2011]";
 
 function referenceExtractor(inputText: string) {
-    const regex = `\[(.+?)\]`
-    const indexOfFirst = inputText.search(regex);
-    console.log(indexOfFirst)
+  
+
+  // Extract all references from the text
+  const rawReferences = inputText.match(referenceIdentificationRegex);
+  if (typeof rawReferences === null) {
+    throw new Error("No matching expressions found.");
+  }
+
+  const extractedReferences: string[] = [];
+
+  // Remove square brackets from all extracted references and append to extractedReferences array
+  rawReferences?.forEach((reference) => {
+    const debracketedReference = reference.replace(squareBracketRegex, "");
+    extractedReferences.push(debracketedReference);
+  });
+
+  return extractedReferences;
 }
 
-referenceExtractor(inputText)
+function referenceLibraryBuilder(extractedReferences: string[]) {
+  extractedReferences.forEach((inputReference) => {
+    // Could be extracted into its own function
+    const referenceExists = referenceLibrary.some((reference) => {
+      if (reference.nameYear === inputReference) {
+        return true;
+      }
+      return false;
+    });
 
+    if (!referenceExists) {
+      const reference: IReference = {
+        id: referenceLibrary.length + 1,
+        nameYear: inputReference,
+      };
+      referenceLibrary.push(reference)
+    }
+  });
+}
+
+
+
+
+
+// Calling the relevant functions
+
+const extractedReferences = referenceExtractor(inputText);
+referenceLibraryBuilder(extractedReferences);
+console.log(referenceLibrary);
 
