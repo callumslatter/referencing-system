4cdd2586b5d865b088b3299bb64a3c1344ec5b3b
Callum Slatter <callum@liftedcare.com>
2022-08-30 21:09:01 +0100

    Refactor and rename variables

--- referencing-system.ts
+++ referencing-system.ts
@@ -13,81 +13,81 @@ const referenceLibrary = new Map();
 const referenceInstanceLibrary = new Map();
 
 // Keep track of groups of references
-let groupOfReferencesForFormatting: number[] = [];
-
-let startOfReferenceIndex: number = 0;
-let startOfSectionIndex: number = 0;
+let arrayOfRefsForFormatting: number[] = [];
 
 // Keep track of the instance being worked on
-declare var referenceInstanceCount: number;
-referenceInstanceCount = 0;
+declare var refInstanceCount: number;
+refInstanceCount = 0;
 
-let lastReferenceToBeCleared: number;
+let lastRefToBeFormatted: number;
 
-let remainingText = "";
-let outputText = "";
-let finalText = "";
+// let remainingText: string = "";
+let finalText: string = "";
 
 // REGEX
-const REFERENCE_IDENTIFICATION_REGEX = /\[(.+?)\]/g;
+const REFERENCE_IDENTIFICATION_REGEX: RegExp = /\[(.+?)\]/g;
 
 // INPUT
 const inputText: string =
   "Mollitia quasi dolorem molestiae ut est voluptates quidem.[Fitt 2011] Natus sit dolore eveniet modi dolores dolore.[Auchinleck 1992] [Wix 2012] Voluptatem vel vel officiis recusandae hic. Sit esse eaque quisquam provident odit et quis nostrum. Dolores ea maiores.[Elcom 2004] [Lindroos 2007] [O'Sheeryne 2004] Iste aut deleniti maiores aliquam asperiores illum consectetur.[Lindroos 2007] [Webben 1987] [Smithin 2004] [Brambill 2001] Ut in et voluptatem sit odit laborum. Veritatis aut reiciendis quasi mollitia esse qui.[Auchinleck 1992] [Wix 2012] [Lindroos 2007] [Smithin 2004] [Brambill 2001] [Varvell 2011]";
 
 function referenceExtractor(inputText: string) {
-  // Keeping track of which reference was at which index
+
+  let remainingText: string = "";
   let instanceCounter = 1;
-  const referenceInstances = identifyReferences(inputText);
-  for (const referenceInstance of referenceInstances) {
-    const newReferenceInstance: IReferenceInstance = {
-      nameYear: referenceInstance[0],
-      index: referenceInstance.index!,
+
+  // Track where each reference occurs
+  const refInstance = identifyReferences(inputText);
+  for (const instance of refInstance) {
+    const newRefInstance: IReferenceInstance = {
+      nameYear: instance[0],
+      index: instance.index!,
     };
-    referenceInstanceLibrary.set(instanceCounter, newReferenceInstance);
+    referenceInstanceLibrary.set(instanceCounter, newRefInstance);
     instanceCounter++;
   }
 
-  console.log(referenceInstanceLibrary);
-  // Run the text through some regex
-  const rawReferences = identifyReferences(inputText);
-
-  // Build a collection of references and their locations
-  for (const rawReference of rawReferences) {
-    if (recordExists(rawReference)) {
-      const existingReference = referenceLibrary.get(rawReference[0]);
-      const existingLocations = existingReference.locations;
-      if (rawReference.index) {
-        const newLocation = rawReference.index;
-        existingLocations.push(newLocation);
-        groupOfReferencesForFormatting.push(existingReference.referenceId + 1);
-      }
+  // Build a collection of reference authors, indexes of their references, and length of each reference
+  // Checks to see if there's a reference directly after the one currently being focused on
+  const rawRefs = identifyReferences(inputText);
+  for (const rawRef of rawRefs) {
+    if (recordExists(rawRef)) {
+      const existingRef = referenceLibrary.get(rawRef[0]);
+      const existingLocations = existingRef.locations;
+      const newLocation = rawRef.index;
+      existingLocations.push(newLocation);
+      arrayOfRefsForFormatting.push(existingRef.referenceId);
     }
-    if (!recordExists(rawReference)) {
-      const newReference: IReference = {
-        referenceId: referenceLibrary.size,
-        locations: [rawReference.index!],
-        referenceLength: rawReference[0].length,
+    else if (!recordExists(rawRef)) {
+      const newRef: IReference = {
+        referenceId: referenceLibrary.size + 1,
+        locations: [rawRef.index!],
+        referenceLength: rawRef[0].length,
       };
-      referenceLibrary.set(rawReference[0], newReference);
-      groupOfReferencesForFormatting.push(newReference.referenceId + 1);
+      referenceLibrary.set(rawRef[0], newRef);
+      arrayOfRefsForFormatting.push(newRef.referenceId);
     }
     remainingText = inputText.slice(
-      rawReference.index! + rawReference[0].length + 1
+      rawRef.index! + rawRef[0].length + 1
     );
-    // startOfReferenceIndex = rawReference.index!;
+
+    // Check text immediately after the current reference. If nothing adjacent, format existing collection of references.
     identifyAdjacentReferences(remainingText);
-    referenceInstanceCount++;
+    refInstanceCount++;
   }
+
   const referencesLeftToFormat =
-    groupOfReferencesForFormatting.length > 0 ? true : false;
+    arrayOfRefsForFormatting.length > 0 ? true : false;
 
   if (referencesLeftToFormat) {
-    clearReferenceGroupAndSaySomething();
+    concatReferences();
   }
 
-  console.log(referenceLibrary);
+  console.log(inputText)
+  console.log("\n")
   console.log(finalText);
+  console.log("\n")
+  createReferenceList(referenceLibrary);
 }
 
 // Helper Functions
@@ -95,8 +95,8 @@ function identifyReferences(text: string) {
   return text.matchAll(REFERENCE_IDENTIFICATION_REGEX);
 }
 
-function recordExists(rawReference: RegExpMatchArray) {
-  const reference = rawReference[0];
+function recordExists(rawRef: RegExpMatchArray) {
+  const reference = rawRef[0];
   if (referenceLibrary.get(reference) === undefined) {
     return false;
   } else {
@@ -109,34 +109,46 @@ function identifyAdjacentReferences(string) {
     return;
   }
   if (!isOpenSquareBracket(string[0])) {
-    console.log("The next character was not a square bracket");
-    clearReferenceGroupAndSaySomething();
+    concatReferences();
   }
 }
 
-function clearReferenceGroupAndSaySomething() {
-  if (referenceInstanceCount === 0) {
+function concatReferences() {
+  let startOfReferenceIndex: number = 0;
+  let startOfSectionIndex: number = 0;
+  // Establish indexes for slice functions
+  if (refInstanceCount === 0) {
     startOfSectionIndex = 0;
     startOfReferenceIndex = referenceInstanceLibrary.get(1).index;
   } else {
     const previousReference = referenceInstanceLibrary.get(
-      lastReferenceToBeCleared
+      lastRefToBeFormatted
     );
     const endOfPreviousReference =
       previousReference.index + previousReference.nameYear.length;
     startOfSectionIndex = endOfPreviousReference;
     startOfReferenceIndex = referenceInstanceLibrary.get(
-      lastReferenceToBeCleared + 1
+      lastRefToBeFormatted + 1
     ).index;
   }
-  outputText = inputText.slice(startOfSectionIndex, startOfReferenceIndex);
-  finalText = finalText.concat(outputText);
-  const formattedGroup = formatGroupedReferences(
-    groupOfReferencesForFormatting
+  // 
+  let textBetweenReferences = inputText.slice(startOfSectionIndex, startOfReferenceIndex);
+  finalText = finalText.concat(textBetweenReferences);
+  const formattedReferences = formatGroupOfReferences(
+    arrayOfRefsForFormatting
   );
-  finalText = finalText.concat(`(${formattedGroup})`);
-  groupOfReferencesForFormatting = [];
-  lastReferenceToBeCleared = referenceInstanceCount + 1;
+  finalText = finalText.concat(`(${formattedReferences})`);
+  arrayOfRefsForFormatting = [];
+  lastRefToBeFormatted = refInstanceCount + 1;
+}
+
+function createReferenceList(referenceLibrary) {
+  console.log("Reference list:")
+  const references = referenceLibrary.entries()
+  for (const reference of references) {
+    const debracketedReference = reference[0].slice(1, reference[0].length - 1)
+    console.log(`${reference[1].referenceId}. ${debracketedReference}`)
+  }
 }
 
 function isWhiteSpace(str) {
@@ -147,47 +159,48 @@ function isOpenSquareBracket(str) {
   return str.match(/[[]/);
 }
 
-// WIPPPPP
-function formatGroupedReferences(groupOfReferencesForFormatting: number[]) {
+function formatGroupOfReferences(arrayOfRefsForFormatting: number[]) {
+  // If less than three references, output references in current state
   if (
-    groupOfReferencesForFormatting.length === 1 ||
-    groupOfReferencesForFormatting.length === 2
+    arrayOfRefsForFormatting.length === 1 ||
+    arrayOfRefsForFormatting.length === 2
   ) {
-    return groupOfReferencesForFormatting.toString();
-  } else if (groupOfReferencesForFormatting.length > 2) {
-    let miniOutputString = "";
-    let numbersInDashedGroup: number[] = [];
-    for (let i = 0; i < groupOfReferencesForFormatting.length; i++) {
-      // Handle end of range situations 
-      if (i + 1 === groupOfReferencesForFormatting.length) {
-        if (groupOfReferencesForFormatting[i] - groupOfReferencesForFormatting[i - 1] === 1) {
-          numbersInDashedGroup.push(groupOfReferencesForFormatting[i])
-          miniOutputString = miniOutputString.concat(`${numbersInDashedGroup[0]}-${numbersInDashedGroup[numbersInDashedGroup.length - 1]}`)
-          numbersInDashedGroup = []
-        } else if (groupOfReferencesForFormatting[i] - groupOfReferencesForFormatting[i - 1] > 1) {
-          // Format the numbersInDashedGroup 
-          miniOutputString = miniOutputString.concat(numbersInDashedGroup.toString())
+    return arrayOfRefsForFormatting.toString();
+  } else if (arrayOfRefsForFormatting.length > 2) {
+    let outputString = "";
+    let refsInDashedGroup: number[] = [];
+    for (let i = 0; i < arrayOfRefsForFormatting.length; i++) {
+      // This 'If' clause handles final ref in arrayOfRefs
+      if (i + 1 === arrayOfRefsForFormatting.length) {
+        // If final ref Id is only 1 more than penultimate...
+        if (arrayOfRefsForFormatting[i] - arrayOfRefsForFormatting[i - 1] === 1) {
+          refsInDashedGroup.push(arrayOfRefsForFormatting[i])
+          outputString = outputString.concat(`${refsInDashedGroup[0]}-${refsInDashedGroup[refsInDashedGroup.length - 1]}`)
+          refsInDashedGroup = []
+          // If final ref Id is more than 1 more than penultimate...
+        } else if (arrayOfRefsForFormatting[i] - arrayOfRefsForFormatting[i - 1] > 1) {
+          outputString = outputString.concat(refsInDashedGroup.toString())
         }
       // Handle in range situations
-      } else if (i + 1 < groupOfReferencesForFormatting.length) {
+      } else if (i + 1 < arrayOfRefsForFormatting.length) {
         // Next reference is only one more than current reference
-        if (groupOfReferencesForFormatting[i + 1] - groupOfReferencesForFormatting[i] === 1)  {
-          numbersInDashedGroup.push(groupOfReferencesForFormatting[i])
+        if (arrayOfRefsForFormatting[i + 1] - arrayOfRefsForFormatting[i] === 1)  {
+          refsInDashedGroup.push(arrayOfRefsForFormatting[i])
         }
         // Next reference is more than one more than current reference
-        if (groupOfReferencesForFormatting[i + 1] - groupOfReferencesForFormatting[i] > 1) {
-          numbersInDashedGroup.push(groupOfReferencesForFormatting[i])
-          if (numbersInDashedGroup.length > 2) {
-            miniOutputString = miniOutputString.concat(`${numbersInDashedGroup[0]}-${numbersInDashedGroup[numbersInDashedGroup.length - 1]},`)
-          numbersInDashedGroup = []
-          } else if (numbersInDashedGroup.length <= 2) {
-            miniOutputString = miniOutputString.concat(`${numbersInDashedGroup.toString()},`)
-            numbersInDashedGroup = []
+        if (arrayOfRefsForFormatting[i + 1] - arrayOfRefsForFormatting[i] > 1) {
+          refsInDashedGroup.push(arrayOfRefsForFormatting[i])
+          if (refsInDashedGroup.length > 2) {
+            outputString = outputString.concat(`${refsInDashedGroup[0]}-${refsInDashedGroup[refsInDashedGroup.length - 1]},`)
+          refsInDashedGroup = []
+          } else if (refsInDashedGroup.length <= 2) {
+            outputString = outputString.concat(`${refsInDashedGroup.toString()},`)
+            refsInDashedGroup = []
           }
         }
       }
     }
-    return miniOutputString;
+    return outputString;
   }
 }
 
