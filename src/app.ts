fe52b514cbbd944fd292b643ec76cd663e116f7c
Callum Slatter <callum@liftedcare.com>
2022-08-31 15:19:49 +0100

    chore: remove additional bodyParser middleware

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,6 @@ import express from "express";
 
 export const app = express();
 
-// Use body parser to read sent json payloads
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
 app.use(bodyParser.json());
 
 RegisterRoutes(app);
